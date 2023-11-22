fa109f3bc62de1c8b3556f406b85205ab52057cd
ThanhLoc1964 <116378502+ThanhLoc1964@users.noreply.github.com>
2023-11-23 00:53:22 +0700

    Create 05. In phần tử.cpp

---
+++ 1D Array/05. In phần tử.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+	int n; cin >> n;
+	int a[n]; for(int &x : a) cin >> x;
+	int x; cin >> x;
+
+	int flag = 0;
+	for(int i = 0 ; i < n ; i+=2) {
+		if(a[i] % 2 == 0) {
+			flag = 1;
+			cout << a[i] << " ";
+		}
+	}
+
+	if(flag == 0) cout << "NONE";
+
+
+	return 0;
+}
