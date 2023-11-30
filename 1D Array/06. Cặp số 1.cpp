25c3950d1f120c601f869c2991845bb40d8c79a1
ThanhLoc1964 <116378502+ThanhLoc1964@users.noreply.github.com>
2023-12-01 00:10:21 +0700

    Update 06. Cặp số 1.cpp

--- 1D Array/06. Cặp số 1.cpp
+++ 1D Array/06. Cặp số 1.cpp
@@ -12,7 +12,7 @@ int main() {
 	int k ; cin >> k;
 
 
-	/* Cach 2: Phep nhan va to hop*/ 
+	/* Cach 3: Phep nhan va to hop*/ 
 	int cnt = 0;
 	for(int x = 0 ; x <= k/2 ; x++) {
 		int y = k - x;
@@ -22,6 +22,33 @@ int main() {
 	cout << cnt;
 
 
+	
+
+	/* Cach 2 %/
+ 	int n; cin >> n;
+	int a[n];
+	map<int, int> mp;
+	for(int &x : a) {
+		cin >> x;
+		mp[x]++;
+	}
+	int k ; cin >> k;
+
+	int cnt = 0;
+	for(int i = 0 ; i < n ; i++) {
+		cnt += mp[k - a[i]];
+		if(a[i] * 2 == k) {
+			cnt--;
+		}
+	}
+
+	cout << cnt / 2;
+	return 0;
+
+
+
+
+ 
 
 	/* Cach 1*/
 	// int cnt = 0;
