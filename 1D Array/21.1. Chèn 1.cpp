aad9ab69819569db9fe709581425d84082881996
ThanhLoc1964 <116378502+ThanhLoc1964@users.noreply.github.com>
2023-12-16 21:03:04 +0700

    Update 21.1. Chèn 1.cpp

--- 1D Array/21.1. Chèn 1.cpp
+++ 1D Array/21.1. Chèn 1.cpp
@@ -10,7 +10,15 @@ int main() {
 	for(int x : a) v.push_back(x);
 
 	v.insert(v.begin() + k - 1, x);
-	for(int x : v) cout << x << " ";	
+	for(int x : v) cout << x << " ";
+
+
+	/* Cách 2 */
+	// for(int i = n - 1; i > k ; i--) {
+	// 	a[i + 1] = a[i];
+	// }
+	// a[k - 1] = x; 
+	// for(int i = 0 ; i < n ; i++) cout << a[i] << " ";
 	
 	return 0;
 }
