bfd20eba5dfa9075f8786614bf0ccbbe74842658
ThanhLoc1964 <116378502+ThanhLoc1964@users.noreply.github.com>
2023-12-18 20:18:53 +0700

    Update 24. Cửa sổ cỡ K.cpp

--- 1D Array/24. Cửa sổ cỡ K.cpp
+++ 1D Array/24. Cửa sổ cỡ K.cpp
@@ -6,13 +6,26 @@ int main() {
 	int a[n];
 	for(int &x : a ) cin >> x;
 
-	for(int i = 0 ; i < n - k + 1 ; i++) {
-		int rs = 0;
-		for(int j = i; j < i + k ; j++) {
-			rs += a[j];
-		}
-		cout << rs << " ";
+	/* Cách 2: Kỹ thuật Cửa sổ trược (Sliding Window) */
+	int sum = 0;
+	for(int i = 0 ; i < k; i++) sum += a[i]; 
+	cout << sum << " ";
+	
+	for(int i = k; i < n; i++) {
+		sum = sum - a[i - k] + a[i];
+		cout << sum << " ";
 	}
+	
+
+	
+	/* Cách 1 O(n * k) */
+	// for(int i = 0 ; i < n - k + 1 ; i++) {
+	// 	int rs = 0;
+	// 	for(int j = i; j < i + k ; j++) {
+	// 		rs += a[j];
+	// 	}
+	// 	cout << rs << " ";
+	// }
 	
 	return 0;
 }
